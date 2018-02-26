0f2f0f34272c6cadacb09406b444b7feb708746b
Krzysztof <musashimiyamoto@interia.eu>
2018-02-26 18:39:02 +0100

    mod_7zad_2

---
+++ actions.js
@@ -0,0 +1,44 @@
+﻿import uuid from "uuid";
+
+export const CREATE_COMMENT = "CREATE_COMMENT";
+export const EDIT_COMMENT = "EDIT_COMMENT";
+export const REMOVE_COMMENT = "REMOVE_COMMENT";
+export const THUMB_UP_COMMENT = "THUMB_UP_COMMENT";
+export const THUMB_DOWN_COMMENT = "THUMB_DOWN_COMMENT";
+
+function createComment(text) {
+  return {
+    type: CREATE_COMMENT,
+    text,
+    id: uuid.v4()
+  };
+}
+
+function editComment(text) {
+  return {
+    type: EDIT_COMMENT,
+    text,
+    id
+  };
+}
+
+function removeComment(text) {
+  return {
+    type: REMOVE_COMMENT,
+    id
+  };
+}
+
+function thumbUpComment(id) {
+  return {
+    type: THUMB_UP_COMMENT,
+    id
+  };
+}
+
+function thumbDownComment(id) {
+  return {
+    type: THUMB_DOWN_COMMENT,
+    id
+  };
+}
